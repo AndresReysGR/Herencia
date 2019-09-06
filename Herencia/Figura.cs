de73b084147604e0e4034d034fd7b06e68dc5eaa
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-09-06 08:45:28 -0700

    Agregar archivos de proyecto.

---
+++ Herencia/Figura.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    abstract class  Figura
+    {
+        public string Color { get; set; }
+        //Todas las figuras deben ser 
+        //capaces de calcular sus areas
+        public abstract double calcularArea();
+        public string toString()
+        {
+            return "Soy una figura: " + Color;
+
+        }
+    }
+}
