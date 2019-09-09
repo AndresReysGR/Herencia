65fa0957dff878e5716523530ff636e4930df74f
lizbethblu04 <lizbethblu04@hotmail.com>
2019-09-09 12:35:04 -0700

    Aqui hice cambios

--- Herencia/Program.cs
+++ Herencia/Program.cs
@@ -10,9 +10,38 @@ namespace Herencia
     {
         static void Main(string[] args)
         {
+            //triangulo
             Triangulo triangulo = new Triangulo();
-            triangulo.Color = "Amarillo";
+            triangulo.Color = "Amarilla";
             Console.WriteLine(triangulo.toString());
+            triangulo.Altura = 2.5;
+            triangulo.Base = 4;
+            double areaTriangulo = triangulo.calcularArea();
+            Console.WriteLine("Area Del Triangulo: " + areaTriangulo);
+
+            //circulo
+            Circulo circulo = new Circulo(5.4);
+            Console.WriteLine("Area del circulo: " + circulo.calcularArea().ToString());
+
+            //Variables de otra forma, al momento de tener clases predeterminadas
+            Figura triangulo2 = new Triangulo();
+
+            ((Triangulo)triangulo2).Base = 2;
+            ((Triangulo)triangulo2).Altura = 3;
+
+            Console.WriteLine("Area del triangulo 2: " + triangulo2.calcularArea().ToString());
+
+            //Forma de enlistar
+            List<Figura> figuras = new List<Figura>();
+
+            figuras.Add(triangulo);
+            figuras.Add(circulo);
+            figuras.Add(triangulo2);
+
+            foreach(var figura in figuras)
+            {
+                Console.WriteLine("Area de figura: " + figura.calcularArea().ToString());
+            }
 
             Console.Read();
         }
